a91130ad0ce797f459443f4f9073cba863c91e41
farooqzehri <farooqahmedbaloch17@gmail.com>
2026-06-09 01:57:27 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req , res) => {
+    res.send("hello World");
+    })
+app.get('/about' , (req , res) => {
+    res.send("About PAge")
+})
+app.get('/contact' , (req , res) => {
+    res.send("contact page")
+})
+app.get('/services' , (req  , res) => {
+    res.send("services page new")
+})
+app.get('/users/:id' , (req , res) => {
+    res.send( req.params.id)
+})
+app.get('/products/:id' , (req , res) => {
+res.send(req.params.id)
+})
+app.get('/items/:id', (req, res)=>{
+    res.send(req.params.id)
+})
+app.get('/search' , (req , res) => {
+    res.send(req.query)
+})
+const logger = (req , res , next) => {
+    console.log(req.method , res.url);
+    next();
+    
+}
+app.use(logger);
+app.listen(port , () => {
+    console.log('response is ready' , port);
+})
