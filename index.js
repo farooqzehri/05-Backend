6db462d7a34a707b624ad705ae572c8b92c5eac8
farooqzehri <farooqahmedbaloch17@gmail.com>
2026-06-15 11:53:13 +0500

    commented all the code

--- index.js
+++ index.js
@@ -5,30 +5,32 @@ const port = 3000;
 app.get('/', (req , res) => {
     res.send("hello World");
     })
-app.get('/about' , (req , res) => {
-    res.send("About PAge")
-})
-app.get('/contact' , (req , res) => {
-    res.send("contact page")
-})
-app.get('/home' , (req , res) => {
-    res.send("Hello I am Farooq Zehri")
-})
-app.get('/services' , (req  , res) => {
-    res.send("services page new")
-})
-app.get('/users/:id' , (req , res) => {
-    res.send( req.params.id)
-})
-app.get('/products/:id' , (req , res) => {
-res.send(req.params.id)
-})
-app.get('/items/:id', (req, res)=>{
-    res.send(req.params.id)
-})
-app.get('/search' , (req , res) => {
-    res.send(req.query)
-})
+
+    
+// app.get('/about' , (req , res) => {
+//     res.send("About PAge")
+// })
+// app.get('/contact' , (req , res) => {
+//     res.send("contact page")
+// })
+// app.get('/home' , (req , res) => {
+//     res.send("Hello I am Farooq Zehri")
+// })
+// app.get('/services' , (req  , res) => {
+//     res.send("services page new")
+// })
+// app.get('/users/:id' , (req , res) => {
+//     res.send( req.params.id)
+// })
+// app.get('/products/:id' , (req , res) => {
+// res.send(req.params.id)
+// })
+// app.get('/items/:id', (req, res)=>{
+//     res.send(req.params.id)
+// })
+// app.get('/search' , (req , res) => {
+//     res.send(req.query)
+// })
 const logger = (req , res , next) => {
     console.log(req.method , res.url);
     next();
